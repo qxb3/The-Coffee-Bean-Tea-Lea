7fb5acdd90aada018ca234e0fabc3296eb82029e
qxb3 <qxbthree@gmail.com>
2023-11-04 01:23:50 +0800

    added heritage timeline page

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -59,7 +59,7 @@
       <img class="absolute top-[-1rem] right-0 w-[26rem] xl:block hidden" src="/ui/home/coffee-cup.png" alt="Coffee Cup" />
 
       <h1 class="h1">The best quality coffee beans <br>for the best coffee brew</h1>
-      <p class="p text-primary-400 mt-6">A cup is enough to give you energy <br>all day long</p>
+      <p class="text-primary-400 mt-6">A cup is enough to give you energy <br>all day long</p>
       <button class="btn sm:btn-lg variant-filled-primary mt-12">
         <span><i class="far fa-shopping-bag"></i></span>
         <span>Order Now</span>
@@ -70,7 +70,7 @@
     <div class="py-16 px-8 block" class:hidden={$page.url.pathname !== "/menu"}>
       <div class="text-center max-w-xl mx-auto">
         <h1 class="h1 underline z-10">Menu</h1>
-        <p class="p text-primary-400 mt-6">Savor an exquisite menu, where every dish embodies culinary perfection. Join us for an unforgettable dining experience that blends passion with exceptional taste.</p>
+        <p class="text-primary-400 mt-6">Savor an exquisite menu, where every dish embodies culinary perfection. Join us for an unforgettable dining experience that blends passion with exceptional taste.</p>
       </div>
     </div>
 
@@ -78,7 +78,15 @@
     <div class="py-16 px-4 block" class:hidden={$page.url.pathname !== "/about"}>
       <div class="text-center max-w-xl mx-auto">
         <h1 class="h1 underline z-10">About Us</h1>
-        <p class="p text-primary-400 mt-6">Discover The Coffee Tea & Lea, where passion for exceptional taste meets a dedication to culinary perfection. Since our inception, we have been committed to curating an exquisite menu that embodies the essence of our craft. Each dish is a reflection of our devotion to the art of gastronomy, meticulously prepared with the finest, handpicked ingredients.</p>
+        <p class="text-primary-400 mt-6">Discover The Coffee Tea & Lea, where passion for exceptional taste meets a dedication to culinary perfection. Since our inception, we have been committed to curating an exquisite menu that embodies the essence of our craft. Each dish is a reflection of our devotion to the art of gastronomy, meticulously prepared with the finest, handpicked ingredients.</p>
+      </div>
+    </div>
+
+    <!-- About > Heritage Header -->
+    <div class="py-16 px-4 block" class:hidden={$page.url.pathname !== "/about/heritage"}>
+      <div class="text-center max-w-xl mx-auto">
+        <h1 class="h1 underline z-10">Our Heritage</h1>
+        <p class="text-primary-400 mt-6">We too come from humble beginnings :)</p>
       </div>
     </div>
 
@@ -86,11 +94,11 @@
     <div class="py-16 px-4 block" class:hidden={$page.url.pathname !== "/resources"}>
       <div class="text-center max-w-xl mx-auto">
         <h1 class="h1 underline z-10">Resources</h1>
-        <p class="p text-primary-400 mt-6">Here you can find useful resources for our fellow coffee lovers!</p>
+        <p class="text-primary-400 mt-6">Here you can find useful resources for our fellow coffee lovers!</p>
       </div>
     </div>
 
-    <!-- Resource Blog Header -->
+    <!-- Resources > Blog Header -->
     <div class="py-4 px-4 block" class:hidden={!$blogStore}>
       <ol class="breadcrumb-nonresponsive justify-center flex-col md:flex-row">
         <li class="crumb">
---
+++ src/routes/about/heritage/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+  const timeline = [
+    {
+      date: '1963',
+      description: 'Our first store in Southern California opened, where founder Herb Hyman began importing and roasting coffee.',
+      img: '/ui/heritage/1963.png'
+    },
+    {
+      date: '1970s',
+      description: 'Herb Hyman moved roasting facility to Camarillo and began to establish direct relationships with coffee growers.',
+      img: '/ui/heritage/1970s.png'
+    },
+    {
+      date: '1987',
+      description: 'History was made when a barista invented the ICE BLENDED® drink at our Westwood, California store.',
+      img: '/ui/heritage/1987.png'
+    },
+    {
+      date: '1988',
+      description: 'The Chai Tea Latte launched.',
+      img: '/ui/heritage/1988.png'
+    },
+    {
+      date: '2003',
+      description: 'We opened our first store in the Philippines.',
+      img: '/ui/heritage/2003.png'
+    }
+  ]
+</script>
+
+<div class="container p-8">
+  {#each timeline as { date, description, img }, i}
+    {@const isEven = i % 2 === 0}
+
+    <div class="grid grid-cols-3">
+      <div class="grid items-center order-1" class:!order-3={isEven}>
+        <img src={img} alt="">
+      </div>
+
+      <div class="grid grid-rows-[1fr_auto_1fr] gap-4 items-center px-4 order-2 h-96">
+        <div class="mx-auto w-1 h-full bg-secondary-500 rounded-t-md" class:invisible={i === 0}></div>
+
+        <div class="grid items-center grid-cols-[1fr_auto_1fr] gap-4">
+          <div class="w-full h-1 bg-secondary-500 rounded-md"></div>
+          <div class="mx-auto w-12 h-12 rounded-full border-4 border-primary-500"></div>
+          <div class="w-full h-1 bg-secondary-500 rounded-md"></div>
+        </div>
+
+        <div class="mx-auto w-1 h-full bg-secondary-500 rounded-md"></div>
+      </div>
+
+      <div class="grid items-center order-3" class:!order-1={isEven}>
+        <div class="max-w-md" class:text-end={isEven}>
+          <h1 class="h1 text-secondary-500">{date}</h1>
+          <p class="mt-2">{description}</p>
+        </div>
+      </div>
+    </div>
+  {/each}
+
+  <div class="text-center mt-12">
+    <h1 class="h1 text-secondary-500">Today</h1>
+    <p>Set out on a journey to take our brand to new heights worldwide.</p>
+
+    <button class="btn variant-filled-secondary rounded-xl mt-6">
+      <span><i class="far fa-scroll"></i></span>
+      <span>Visit our menu</span>
+    </button>
+  </div>
+</div>
