a53e19737c0d7bb060eafcf17d44d6c3c77496c2
qxb3 <qxbthree@gmail.com>
2023-11-05 00:59:04 +0800

    added apply job page

---
+++ src/routes/apply-job/+page.svelte
@@ -0,0 +1,71 @@
+<div class="container p-8">
+  <h1 class="h1">Apply Now</h1>
+
+  <form class="mt-12 space-y-4" action="">
+    <label class="label">
+      <span class="h5">Full Name</span>
+      <input class="input" type="text" placeholder="Full Name" />
+    </label>
+
+    <label class="label">
+      <span class="h5">Email</span>
+      <input class="input" type="text" placeholder="Email Adress" />
+    </label>
+
+    <label class="label">
+      <span class="h5">Mobile Number</span>
+      <input class="input" type="text" placeholder="i.e +6312345678" />
+    </label>
+
+    <label class="label">
+      <span class="h5">Town / City</span>
+      <input class="input" type="text" placeholder="Quezon City" />
+    </label>
+
+    <label class="label">
+      <span class="h5">Position</span>
+      <select class="select">
+        <option value="" disabled="" selected="">Select your position</option>
+        <option value="Commissary - Warehouse Manager"> Commissary - Warehouse Manager</option>
+        <option value="Commissary - R&amp;D Supervisor"> Commissary - R&amp;D Supervisor</option>
+        <option value="Commissary - Warehouse Assistant"> Commissary - Warehouse Assistant</option>
+        <option value="Commissary - Production Assistant"> Commissary - Production Assistant</option>
+        <option value="Commissary - Baker"> Commissary - Baker</option>
+        <option value="Commissary - Driver"> Commissary - Driver</option>
+        <option value="Finance &amp; Strategic Planning Department - Finance Controller"> Finance &amp; Strategic Planning Department - Finance Controller</option>
+        <option value="Finance &amp; Strategic Planning Department - Tax Officer"> Finance &amp; Strategic Planning Department - Tax Officer</option>
+        <option value="Finance &amp; Strategic Planning Department - Inventory Officer"> Finance &amp; Strategic Planning Department - Inventory Officer</option>
+        <option value="Finance &amp; Strategic Planning Department - Financial Reporting Supervisor"> Finance &amp; Strategic Planning Department - Financial Reporting Supervisor</option>
+        <option value="Finance &amp; Strategic Planning Department - Accounts Payable Analyst"> Finance &amp; Strategic Planning Department - Accounts Payable Analyst</option>
+        <option value="Marketing - Marketing Manager"> Marketing - Marketing Manager</option>
+        <option value="Marketing - Marketing Officer"> Marketing - Marketing Officer</option>
+        <option value="Marketing - Sales Manager"> Marketing - Sales Manager</option>
+        <option value="Marketing - Sales Executive"> Marketing - Sales Executive</option>
+        <option value="Marketing - Brand Specialist"> Marketing - Brand Specialist</option>
+        <option value="People &amp; Organization - Organizational Wellness &amp; Development Officer"> People &amp; Organization - Organizational Wellness &amp; Development Officer</option>
+        <option value="People &amp; Organization - Sourcing Specialist"> People &amp; Organization - Sourcing Specialist</option>
+        <option value="People &amp; Organization - Recruitment Specialist"> People &amp; Organization - Recruitment Specialist</option>
+        <option value="Operations - Store Manager"> Operations - Store Manager</option>
+        <option value="Operations - Restaurant Manager"> Operations - Restaurant Manager</option>
+        <option value="Operations - Kitchen Manager"> Operations - Kitchen Manager</option>
+        <option value="Operations - Line Cook"> Operations - Line Cook</option>
+        <option value="Operations - Dining Staff"> Operations - Dining Staff</option>
+        <option value="Operations - Utility Custodian / Housekeeper"> Operations - Utility Custodian / Housekeeper</option>
+        <option value="Operations - Junior Technician / Pest Control Technician"> Operations - Junior Technician / Pest Control Technician</option>
+        <option value="Facilities Management - Facilities Engineer (Cavite)"> Facilities Management - Facilities Engineer (Cavite)</option>
+      </select>
+    </label>
+
+    <label class="label">
+      <span class="h5">Resume</span>
+      <input class="input" type="file" />
+      <span class="text-xs text-zinc-500">Allowed file types: .pdf, .doc, .docx, .jpeg, .jpg, .png Max file size: 5mb</span>
+    </label>
+
+    <label class="label pt-4 space-y-8">
+      <input class="checkbox" type="checkbox" />
+      <span>By clicking submit, you agree to our Privacy Policy. The Coffee Bean and Tea Leaf® will use your personal data to help us respond and contact you regarding your job application. For more information on how we use your data.</span>
+      <button class="btn variant-filled-primary rounded-md mt-4" type="submit">Send Application</button>
+    </label>
+  </form>
+</div>
