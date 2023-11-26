2ef2a3abd27d4f1c14587a0cf5ad18e12b4e4b16
qxb3 <qxbthree@gmail.com>
2023-11-26 17:41:47 +0800

    auth: added login / signup page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+  import { getToastStore } from '@skeletonlabs/skeleton'
+
+  export let data
+  const { supabase } = data
+
+  const toastStore = getToastStore()
+
+  let email = ''
+  let password = ''
+
+  let emailErr = false
+  let passwordErr = false
+
+  async function login() {
+    if (!email || !password) {
+      toastStore.trigger({
+        message: 'Please fill-up missing fields',
+        background: "bg-error-500"
+      })
+    }
+
+    if (!email) return emailErr = true
+    if (!password) return passwordErr = true
+
+    toastStore.trigger({
+      message: 'Logging in! Please wait...',
+      background: 'bg-success-500'
+    })
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) {
+      emailErr = true
+      passwordErr = true
+
+      return toastStore.trigger({
+        message: error.message,
+        background: "bg-error-500"
+      })
+    }
+
+    if (data) {
+      emailErr = false
+      passwordErr = false
+
+      toastStore.trigger({
+        message: 'Successfuly Logged in!',
+        background: 'bg-success-500'
+      })
+    }
+  }
+</script>
+
+<div class="container max-w-xl px-8 py-12">
+  <form class="space-y-6">
+    <label class="label">
+      <span class="h5">Email:</span>
+      <input bind:value={email} class="input" class:input-error={emailErr} type="text" placeholder="qxb3@gmail.com" />
+    </label>
+
+    <label class="label">
+      <span class="h5">Password:</span>
+      <input bind:value={password} class="input" class:input-error={passwordErr} type="password" placeholder="************" />
+    </label>
+
+    <button on:click={login} class="btn variant-filled-success w-full" type="submit">Login</button>
+
+    <p class="text-xs text-center">
+      <span>Don't have an account yet?</span>
+      <span class="link">
+        <a href="/signup">SignUp!</a>
+      </span>
+    </p>
+  </form>
+</div>
