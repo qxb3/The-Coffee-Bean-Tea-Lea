35f39b2bdac58c3635714934b8d89d252308779f
qxb3 <qxbthree@gmail.com>
2023-12-01 02:48:29 +0800

    auth: finish up (2)

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store'
-import { localStorageStore } from '@skeletonlabs/skeleton'
 
 export const blogStore = writable()
 export const cartStore = writable([])
-export const purchasesStore = localStorageStore('purchaces', [])
+export const purchasesStore = writable([])
